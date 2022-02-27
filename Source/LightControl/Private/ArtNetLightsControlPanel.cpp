2491235abb8ddae79bc85fbbe71eeff1ea3a7f7c
Moritz Heinemann <mo@blog-srv.net>
2022-02-28 00:08:50 +0100

    add control panel building

---
+++ Source/LightControl/Private/ArtNetLightsControlPanel.cpp
@@ -0,0 +1,1 @@
+#include "ArtNetLightsControlPanel.h"
---
+++ Source/LightControl/Public/ArtNetLightsControlPanel.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Buildables/FGBuildableControlPanelHost.h"
+#include "Buildables/FGBuildableLightSource.h"
+#include "ArtNetLightsControlPanel.generated.h"
+
+UCLASS()
+class LIGHTCONTROL_API AArtNetLightsControlPanel : public AFGBuildableControlPanelHost
+{
+    GENERATED_BODY()
+public:
+
+private:
+
+};
